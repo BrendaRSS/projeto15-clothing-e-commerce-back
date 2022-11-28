77ac800f6bb3c772d4f842dede8ea0c1d0bd16be
Bruno Baroni <brunobaronims@gmail.com>
2022-11-28 02:48:25 -0300

    Add get one product endpoint

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,23 +1,37 @@
-import {collectionCategories} from "../database/db.js";
+import { ObjectId } from "mongodb";
+import { collectionCategories } from "../database/db.js";
+import { collectionInventory } from "../database/db.js";
 
-export async function postCategories(req, res){
+export async function postCategories(req, res) {
     const body = req.body;
 
-    try{
+    try {
         await collectionCategories.insertOne(body);
         return res.sendStatus(201);
-    } catch(error){
+    } catch (error) {
         console.log(error);
         return res.sendStatus(500);
     }
 }
 
-export async function getCategories(req, res){
-    try{
+export async function getCategories(req, res) {
+    try {
         const hallCategories = await collectionCategories.find().toArray();
         return res.status(200).send(hallCategories);
-    } catch(error){
+    } catch (error) {
         console.log(error);
+        return res.sendStatus(500);
+    }
+}
+
+export async function getOneProduct(req, res) {
+    const productId = new ObjectId(req.params.id);
+
+    try {
+        const productData = await collectionInventory.findOne({ _id: productId })
+        return res.status(200).send(productData);
+    } catch (e) {
+        console.error(e);
         return res.sendStatus(500);
     }
 }
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import {postCategories, getCategories} from "../controllers/productsControllers.js";
-import {validationCategoryMdd} from "../middlewares/validationCategoryMdd.js";
+import {
+  postCategories,
+  getCategories,
+  getOneProduct
+} from "../controllers/productsControllers.js";
+import { validationCategoryMdd } from "../middlewares/validationCategoryMdd.js";
 
 const router = Router();
 
 router.post("/categories", validationCategoryMdd, postCategories);
 
 router.get("/categories", getCategories);
+
+router.get('/products/:id', getOneProduct);
 
 export default router;
